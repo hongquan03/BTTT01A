297cdbb3df8b427a090a243c7f43edae4aab7a09
Hồng Quân <hongquan2443@gmail.com>
2023-09-19 20:48:59 +0700

    Bai 14

--- BTTH01A/index.php
+++ BTTH01A/index.php
@@ -408,6 +408,29 @@ foreach ($numbers as $number) {
 echo "<br>";
 ?>
 
+<!-- Bài 14 -->
+<br>
+<br>
+<span style="color: red;">Bài 14</span>
+    <br>
+    <?php
+$array1 = [
+    [77, 87],
+    [23, 45]
+];
+
+$array2 = [
+    'giá trị 1', 'giá trị 2'
+];
+
+$result = [];
+
+foreach ($array2 as $key => $value) {
+    $result[$key] = array_merge([$value], $array1[$key]);
+}
+
+print_r($result);
+?>
 
 </body>
 </html>
